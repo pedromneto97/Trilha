0c0747bb994e229723fb9ab8dd4352975f854b8c
Pedro Neto <pedromneto97@gmail.com>
2018-07-20 16:39:12 -0300

    Correções na lógica do jogo. Remover passou a ser uma flag no tabuleiro

--- Trilha.py
+++ Trilha.py
@@ -44,7 +44,7 @@ class Trilha(Game):
                     else:
                         self.adjacentes.update({x: [x - 8, x - 1, x + 1]})
         """Estado inicial"""
-        self.initial = GameState(to_move="BRANCO", utility=0, board={"BRANCO": 0, "PRETO": 0}, moves=vazio)
+        self.initial = GameState(to_move="BRANCO", utility=0, board={"BRANCO": 0, "PRETO": 0, "REMOVE": 0}, moves=vazio)
 
     def actions(self, state):
         """Retorna todos os movimentos possíveis no momento"""
@@ -60,49 +60,47 @@ class Trilha(Game):
         utility = 0
         # Jogadas
         moves = []
+        # Lista para não procurar no tabuleiro
+        lista = ["BRANCO", "PRETO", "REMOVE"]
         # Procura uma substring e verifica caso seja a vez de alguém remover a peça
-        p = str.find(state.to_move, "_REMOVE")
-        if p != -1:
+        if state.board['REMOVE'] == 1:
             # Remove a peça do tabuleiro
             del (board[move])
             # Define quem é o próximo a jogar
-            if state.to_move[:p] == "BRANCO":
-                to_move = "PRETO"
-            else:
-                to_move = "BRANCO"
+            board['REMOVE'] = 0
             # Verifica se alguém ganhou com essa jogada
-            utility = self.compute_utility(board, state.to_move[:p])
-        # Caso não seja para remover a peça
-        else:
+            utility = self.compute_utility(board, state.to_move)
             # Verifica ainda tem peça para jogar no tabuleiro
-            if board[state.to_move] < 9:
-                board[move] = state.to_move
-                board[state.to_move] = board[state.to_move] + 1
-            # Caso já tenham sido jogadas as peças, então é para mover uma peça no tabuleiro
+        elif board[state.to_move] < 9:
+            board[move] = state.to_move
+            board[state.to_move] = board[state.to_move] + 1
+        # Caso já tenham sido jogadas as peças, então é para mover uma peça no tabuleiro
+        else:
+            # Define o destino
+            board[move[1]] = state.to_move
+            # Remove da origem
+            del (board[move[0]])
+            move = move[1]
+        # Define quem é o próximo a jogar
+        if state.to_move == "BRANCO":
+            if state.board['REMOVE'] == 0 and self.verifica_trinca(board=board, to_move=state.to_move, move=move):
+                to_move = "BRANCO"
+                board['REMOVE'] = 1
             else:
-                # Define o destino
-                board[move[1]] = state.to_move
-                # Remove da origem
-                del (board[move[0]])
-            # Define quem é o próximo a jogar
-            if state.to_move == "BRANCO":
-                if self.verifica_trinca(state, move):
-                    to_move = "BRANCO_REMOVE"
-                else:
-                    to_move = "PRETO"
+                to_move = "PRETO"
+        else:
+            if state.board['REMOVE'] == 0 and self.verifica_trinca(board=board, to_move=state.to_move, move=move):
+                to_move = "PRETO"
+                board['REMOVE'] = 1
             else:
-                if self.verifica_trinca(state, move):
-                    to_move = "PRETO_REMOVE"
-                else:
-                    to_move = "BRANCO"
-        # Define os próximos movimentos
-        p = str.find(to_move, "_REMOVE")
+                to_move = 'BRANCO'
+
         # Caso o próximo movimento seja de remover
-        if p != -1:
+        if board['REMOVE'] == 1:
             # POS é o número no tabuleiro e to é quem jogou
             for pos, to in board.items():
                 # Caso o estado seja "BRANCO_REMOVE" procura todas as peças preta
-                if not to == to_move[:p]:
+                if not to == to_move and pos not in lista:
                     moves.append(pos)
         # Caso o próximo movimento seja só colocar uma peça no tabuleiro
         elif board[to_move] < 9:
@@ -112,7 +110,7 @@ class Trilha(Game):
             jogadas = []
             vazios = self.vazios(board)
             for pos, to in board.items():
-                if to == to_move:
+                if to == to_move and pos not in lista:
                     # Salva todas as jogadas do próximo a mover a peça
                     jogadas.append(pos)
             # Verifica cada jogada
@@ -140,61 +138,69 @@ class Trilha(Game):
         if board[player] < 9:
             return 0
         cont = 0
+        lista = ["BRANCO", "PRETO", "REMOVE"]
         for pos, to in board.items():
-            if to == player:
+            if not to == player and pos not in lista:
                 cont = cont + 1
         if cont < 3:
-            return -1 if player == "BRANCO" else +1
+            return 1 if player == "BRANCO" else -1
         return 0
 
-    def verifica_trinca(self, state, move):
+    def verifica_trinca(self, board, to_move, move):
         """Verifica se houve alguma trinca com o movimento"""
-        adjacente = self.adjacentes[move]
         # Caso move seja ímpar
         if (move % 2) == 1:
             # Verifica se é 1,9 ou 17
             if (move % 8) == 1:
-                if ((move + 1) in state.board.keys() and state.board[move + 1] == state.to_move and (
-                        move + 2) in state.board.keys() and state.board[move + 2] == state.to_move) or (
-                        (move + 7) in state.board.keys() and state.board[move + 7] == state.to_move and (
-                        move + 6) in state.board.keys() and state.board[move + 6] == state.to_move):
+                if ((move + 1) in board.keys() and board[move + 1] == to_move and (
+                        move + 2) in board.keys() and board[move + 2] == to_move) or (
+                        (move + 7) in board.keys() and board[move + 7] == to_move and (
+                        move + 6) in board.keys() and board[move + 6] == to_move):
                     return True
                 else:
                     return False
-            elif ((move - 1) in state.board.keys() and state.board[move - 1] == state.to_move and (
-                    move - 2) in state.board.keys() and state.board[move - 2] == state.to_move) or (
-                    (move + 1) in state.board.keys() and state.board[move + 1] == state.to_move and (
-                    move + 2) in state.board.keys() and state.board[move + 2] == state.to_move):
+            elif (move % 8) == 7:
+                if ((move + 1) in board.keys() and board[move + 1] == to_move and (
+                        move - 6) in board.keys() and board[move - 6] == to_move) or (
+                        (move - 1) in board.keys() and board[move - 1] == to_move and (
+                        move - 2) in board.keys() and board[move - 2] == to_move):
+                    return True
+                else:
+                    return False
+            elif ((move - 1) in board.keys() and board[move - 1] == to_move and (
+                    move - 2) in board.keys() and board[move - 2] == to_move) or (
+                    (move + 1) in board.keys() and board[move + 1] == to_move and (
+                    move + 2) in board.keys() and board[move + 2] == to_move):
                 return True
             else:
                 return False
         # Caso par
         else:
             if (move % 8) == 0:
-                if (move - 7) in state.board.keys() and state.board[move - 7] == state.to_move and (
-                        move - 1) in state.board.keys() and state.board[move - 1] == state.to_move:
+                if (move - 7) in board.keys() and board[move - 7] == to_move and (
+                        move - 1) in board.keys() and board[move - 1] == to_move:
                     return True
-            elif (move + 1) in state.board.keys() and state.board[move + 1] == state.to_move and (
-                    move - 1) in state.board.keys() and state.board[move - 1] == state.to_move:
+            elif (move + 1) in board.keys() and board[move + 1] == to_move and (
+                    move - 1) in board.keys() and board[move - 1] == to_move:
                 return True
 
             if move <= 8:
-                if (move + 8) in state.board.keys() and state.board[move + 8] == state.to_move and (
-                        move + 16) in state.board.keys() and state.board[move + 16] == state.to_move:
+                if (move + 8) in board.keys() and board[move + 8] == to_move and (
+                        move + 16) in board.keys() and board[move + 16] == to_move:
                     return True
                 else:
                     return False
             # De 9 até 16
             elif move > 8 and move <= 16:
-                if (move - 8) in state.board.keys() and state.board[move - 8] == state.to_move and (
-                        move + 8) in state.board.keys() and state.board[move + 8] == state.to_move:
+                if (move - 8) in board.keys() and board[move - 8] == to_move and (
+                        move + 8) in board.keys() and board[move + 8] == to_move:
                     return True
                 else:
                     return False
             # De 17 até 24
             else:
-                if (move - 8) in state.board.keys() and state.board[move - 8] == state.to_move and (
-                        move - 16) in state.board.keys() and state.board[move - 16] == state.to_move:
+                if (move - 8) in board.keys() and board[move - 8] == to_move and (
+                        move - 16) in board.keys() and board[move - 16] == to_move:
                     return True
                 else:
                     return False
